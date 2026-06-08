54413e385237a93b32d0c8f096df054bcfff579c
clara <clara.brehon@gmail.com>
2026-06-08 13:45:39 +0200

    Update level.ts

--- Mini_jeux/src/lib/level.ts
+++ Mini_jeux/src/lib/level.ts
@@ -23,108 +23,111 @@ const levels: Level[] = [
 { game: "game10", difficulty: "hard", title: "Cyber Nexus" },
 
 //8
-{ game: "game20", difficulty: "hard", title: "Slot the Darts" },
+{ game: "game2", difficulty: "hard", title: "Color Hack" },
 
 //9
-{ game: "game26", difficulty: "hard", title: "Color Chaos" },
+{ game: "game20", difficulty: "easy", title: "Slot the Darts" },
 
 //10
-{ game: "game21", difficulty: "hard", title: "Debugging Duel" },
+{ game: "game26", difficulty: "hard", title: "Color Chaos" },
 
 //11
-{ game: "game12", difficulty: "hard", title: "Pixel Peril" },
+{ game: "game21", difficulty: "hard", title: "Debugging Duel" },
 
 //12
-{ game: "game15", difficulty: "hard", title: "Fast Cars" },
+{ game: "game12", difficulty: "hard", title: "Pixel Peril" },
 
 //13
-{ game: "TransitionMedium", difficulty: "medium", title: "Transition Medium" },
+{ game: "game15", difficulty: "hard", title: "Fast Cars" },
 
 //14
-{ game: "game4", difficulty: "medium", title: "Mysterious Enigma" },
+{ game: "TransitionMedium", difficulty: "medium", title: "Transition Medium" },
 
 //15
-{ game: "game38", difficulty: "hard", title: "Cyber Diavola" },
+{ game: "game4", difficulty: "medium", title: "Mysterious Enigma" },
 
 //16
-{ game: "game7", difficulty: "hard", title: "Polary Memory" },
+{ game: "game38", difficulty: "hard", title: "Cyber Diavola" },
 
 //17
-{ game: "game28", difficulty: "hard", title: "TriadCode" },
+{ game: "game7", difficulty: "hard", title: "Polary Memory" },
 
 //18
-{ game: "game5", difficulty: "medium", title: "Strange Symbols" },
+{ game: "game28", difficulty: "hard", title: "TriadCode" },
 
 //19
-{ game: "game35", difficulty: "hard", title: "Labyrinthe Neural" },
+{ game: "game5", difficulty: "medium", title: "Strange Symbols" },
 
 //20
-{ game: "game39", difficulty: "hard", title: "Ice Stack Deluxe" },
+{ game: "game35", difficulty: "hard", title: "Labyrinthe Neural" },
 
 //21
-{ game: "game29", difficulty: "hard", title: "LetterPulse" },
+{ game: "game39", difficulty: "hard", title: "Ice Stack Deluxe" },
 
 //22
-{ game: "game36", difficulty: "hard", title: "Pac Trap" },
+{ game: "game29", difficulty: "hard", title: "LetterPulse" },
 
 //23
-{ game: "game33", difficulty: "hard", title: "Retro Sequence" },
+{ game: "game36", difficulty: "hard", title: "Pac Trap" },
 
 //24
-{ game: "game24", difficulty: "hard", title: "Framework Frenzy" },
+{ game: "game33", difficulty: "hard", title: "Retro Sequence" },
 
 //25
-{ game: "game16", difficulty: "hard", title: "Galaxy Seum" },
+{ game: "game24", difficulty: "hard", title: "Framework Frenzy" },
 
 //26
-{ game: "game32", difficulty: "hard", title: "Silent Break" },
+{ game: "game16", difficulty: "hard", title: "Galaxy Seum" },
 
 //27
-{ game: "TransitionHard", difficulty: "hard", title: "Transition Hard" },
+{ game: "game32", difficulty: "hard", title: "Silent Break" },
 
 //28
-{ game: "game3", difficulty: "easy", title: "Invisible Maze" },
+{ game: "TransitionHard", difficulty: "hard", title: "Transition Hard" },
 
 //29
-{ game: "game19", difficulty: "hard", title: "Logic Mirrors" },
+{ game: "game3", difficulty: "easy", title: "Invisible Maze" },
 
 //30
-{ game: "game27", difficulty: "hard", title: "Digit Riddle" },
+{ game: "game19", difficulty: "hard", title: "Logic Mirrors" },
 
 //31
-{ game: "game9", difficulty: "hard", title: "Matchy Matchy" },
+{ game: "game27", difficulty: "hard", title: "Digit Riddle" },
 
 //32
-{ game: "game34", difficulty: "hard", title: "End of Reason" },
+{ game: "game9", difficulty: "hard", title: "Matchy Matchy" },
 
 //33
-{ game: "game14", difficulty: "hard", title: "Giant Snake" },
+{ game: "game34", difficulty: "hard", title: "End of Reason" },
 
 //34
-{ game: "game40", difficulty: "hard", title: "Chromix" },
+{ game: "game14", difficulty: "hard", title: "Giant Snake" },
 
 //35
-{ game: "game13", difficulty: "hard", title: "Escape" },
+{ game: "game40", difficulty: "hard", title: "Chromix" },
 
 //36
-{ game: "game23", difficulty: "hard", title: "Compiler Chaos" },
+{ game: "game13", difficulty: "hard", title: "Escape" },
 
 //37
-{ game: "game22", difficulty: "hard", title: "Syntax Siege" },
+{ game: "game23", difficulty: "hard", title: "Compiler Chaos" },
 
 //38
-{ game: "game17", difficulty: "hard", title: "Prime Stickler" },
+{ game: "game22", difficulty: "hard", title: "Syntax Siege" },
 
 //39
-{ game: "game37", difficulty: "hard", title: "Neo Snake" },
+{ game: "game17", difficulty: "hard", title: "Prime Stickler" },
 
 //40
-{ game: "game30", difficulty: "hard", title: "The Unpattern" },
+{ game: "game37", difficulty: "hard", title: "Neo Snake" },
 
 //41
-{ game: "game11", difficulty: "hard", title: "Digital Dawn" },
+{ game: "game30", difficulty: "hard", title: "The Unpattern" },
 
 //42
+{ game: "game11", difficulty: "hard", title: "Digital Dawn" },
+
+//43
 { game: "TransitionEnd", difficulty: "end", title: "Fin du Jeu" },
 
 
